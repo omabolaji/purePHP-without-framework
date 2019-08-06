d5658bfd0f1a0378781066c43ca4c847e5bf3a66
omabolaji <oyewumibolajimuftau@gmail.com>
2019-08-06 10:51:25 -0800

    adding files

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog Home - Start Bootstrap Template</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+</head>
+<body>
+    
+
+
+
+
+
+
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ post.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog Home - Start Bootstrap Template</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+</head>
+<body>
+    
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <!-- <div class="container"> -->
+   <!-- <div class="navbar-header"> -->
+      <a href="#" class="navbar-brand">Navbars</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-control="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      <!-- <span class="navbar-toggler-icon"></span> -->
+      <!-- <span class="navbar-toggler-icon"></span> -->
+    </button>
+     <!-- <a href="#" class="navbar-brand">Start bootstrap</a> -->
+   <!-- </div> -->
+     <div class="collapse navbar-collapse" id="navbarNavDropdown">
+       <ul class="navbar-nav">
+         <li class="nav-item active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+         <li class="nav-item"><a href="#">About</a></li>
+         <li class="nav-item"><a href="#">Service</a></li>
+         <li class="nav-item"><a href="#">Contact</a></li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown link</a> 
+             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+               <a href="#" class="dropdown-item">Action</a>
+               <a href="#" class="dropdown-item">Action1</a>
+               <a href="#" class="dropdown-item">Action2</a>
+               <a href="#" class="dropdown-item">Action4</a>
+             </div>         
+          </li>
+       </ul>
+     </div>
+  <!-- </div> -->
+ </nav>
+
+
+
+
+
+
+    <script src="js/bootstrap.min.js"></script>
+    <!-- <script src="js/bootstrap.bundle.min.js"></script> -->
+</body>
+</html>
